5339b83ce624b0276ed8e66ce82aa5f67530d6e5
Adam Kelly <adamkelly2201@gmail.com>
2021-08-06 15:38:10 +0100

    Use name length instead of redacting

--- index.js
+++ index.js
@@ -2,17 +2,6 @@
 // all Cages as the globally-scoped `evervault` object.This allows you to encrypt the result, 
 // and store it in your database.
 
-function createWelcomeMessage(name) {
-  const formattedName = name.split(' ').map(partialName => {
-    if (partialName.length > 1) {
-      return partialName[0] + '*'.repeat(partialName.length - 2) + partialName.slice(-1);
-    }
-    return partialName
-  }).join(' ');
-
-  return `Hello, ${formattedName}!`
-}
-
 // `data` is the data you encrypted and passed into `evervault.run` from your server. The cage 
 // automatically decrypts the data and maintains its structure so you can treat event exactly as 
 // you did when you passed it into `evervault.run`.
@@ -24,7 +13,7 @@ exports.handler = async (data) => {
     // Process the decrypted name value, and re-encrypt the original name using the globally available evervault package.
     // Note all cages have the evervault SDK automatically injected into their global scope.
     return {
-      message: createWelcomeMessage(data.name),
+      message: `Hello from a cage! It seems you have ${data.name.length} letters in your name`,
       name: await evervault.encrypt(data.name),
     };
   } else {
